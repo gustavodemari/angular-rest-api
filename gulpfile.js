4707a72c01774fd8d39d4ded9462996f00dcf4ba
gustavodemari <guhdemari@gmail.com>
2014-07-18 23:58:45 -0300

    adding build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,45 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-uglify');
+
 
 gulp.task('js', function(){
   //Gerar javascript das libs (angular, bootstrap e etc)
+  gulp.src([
+    'app/bower_components/jquery/dist/jquery.min.js',
+    'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'app/bower_components/angular/angular.min.js',
+    'app/bower_components/angular-route/angular-route.min.js'
+    ])
+  .pipe(concat('libs.js'))
+  .pipe(minify())
+  .pipe(gulp.dest('app/js'))
 })
 
 gulp.task('css', function(){
   //Gerar CSS
+  gulp.src([
+    'app/bower_components/bootstrap/dist/css/bootstrap.min.css'
+    ])
+  .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('fonts', function(){
+  //Gerar fonts
+  gulp.src([
+    'app/bower_components/bootstrap/dist/fonts/**'
+    ])
+  .pipe(gulp.dest('app/fonts'))
 })
 
 gulp.task('app', function(){
   //Gerar javascript do app
+  gulp.src([
+    'app/controllers/**'
+    ])
+  .pipe(concat('app.js'))
+  .pipe(minify())
+  .pipe(gulp.dest('app/js'))
 })
 
-gulp.task('build', ['js', 'css', 'app']);
+gulp.task('build', ['js', 'css', 'fonts', 'app']);
