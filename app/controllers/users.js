5b9dbd536455704d8c48cb24f264810edb23051a
gustavodemari <guhdemari@gmail.com>
2014-07-21 16:42:09 -0300

    adding users controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,1 +1,6 @@
 /* Users Controller */
+var UsersCtrl = angular.module('angular-rest-api.usersController', []);
+
+UsersCtrl.controller('UsersCtrl', ['$scope', function($scope){
+  $scope.users = [{name: 'Gustavo'}, {name: 'Bruna'}];
+}]);
