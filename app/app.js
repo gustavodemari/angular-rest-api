fbef9a78556d3298acffb11eca0b1865df292f7c
gustavodemari <guhdemari@gmail.com>
2014-07-21 16:52:22 -0300

    adding main module and routes

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
 /* Main App Angular*/
+var app = angular.module('angular-rest-api',[
+  'ngRoute',
+  'angular-rest-api.usersController'
+  ]);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/users', {templateUrl:'views/users.html', controller: 'UsersCtrl'});
+}])
